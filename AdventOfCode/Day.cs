e140ca6516db5ee3e9221de81c5462a8440ac0ed
AlekMichael123 <alekmichael2345@gmail.com>
2026-01-18 18:41:21 -0800

    Add day infastructure and program

---
+++ AdventOfCode/Day.cs
@@ -0,0 +1,7 @@
+﻿namespace Advent_Of_Code_CS.AdventOfCode;
+
+public abstract class Day
+{
+  public abstract void Part1(string input);
+  public abstract void Part2(string input);
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Advent_Of_Code_CS.AdventOfCode;
+using Advent_Of_Code_CS.AdventOfCode.Year.AOC2025;
+
+Dictionary<string, Dictionary<string, Day>> solutions = new()
+{
+  { 
+    "2025", 
+    new () {
+      { "1",  new Day1() },
+    } 
+  },
+};
+
+var year = args[0];
+var day = args[1];
+
+Console.WriteLine($"Running Year {year} Day {day}");
+
+var input = File.ReadAllText("input.txt");
+
+
+Console.WriteLine("Part 1");
+solutions[year][day].Part1(input);
+Console.WriteLine("Part 2");
+solutions[year][day].Part2(input);

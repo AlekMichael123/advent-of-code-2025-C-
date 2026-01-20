e2fda58b362ad627df8feeb68596a2e5768e5473
AlekMichael123 <alekmichael2345@gmail.com>
2026-01-20 00:38:43 -0800

    2025 day 2 part 1

--- AdventOfCode/Year/AOC2025/Day2.cs
+++ AdventOfCode/Year/AOC2025/Day2.cs
@@ -4,11 +4,31 @@ public class Day2 : Day
 {
   public override void Part1(string input)
   {
-    throw new NotImplementedException();
+    var parsedInput = _parseInput(input);
+    long result = 0;
+    foreach (var (start, end) in parsedInput)
+    {
+      for (var i = long.Parse(start); i <= long.Parse(end); i++)
+      {
+        if (_isInvalid(i.ToString())) result += i;
+      }
+    }
+    
+    Console.WriteLine($"Sum of all invalid IDs: {result}");
   }
 
   public override void Part2(string input)
   {
     throw new NotImplementedException();
   }
+
+  private (string start, string end)[] _parseInput(string input) =>
+    input
+      .Split(',')
+      .Select(e => e.Split('-'))
+      .Select(e => (first: e.First(), second: e.Last()))
+      .ToArray();
+
+  private bool _isInvalid(string value) =>
+    value[0..(value.Length / 2)] == value[(value.Length / 2)..];
 }

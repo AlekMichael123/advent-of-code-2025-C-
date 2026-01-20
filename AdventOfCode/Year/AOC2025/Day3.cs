95c19a351175c2527f84289359e0469c0566bd0c
AlekMichael123 <alekmichael2345@gmail.com>
2026-01-20 02:53:32 -0800

    2025 day 3 part 1

--- AdventOfCode/Year/AOC2025/Day3.cs
+++ AdventOfCode/Year/AOC2025/Day3.cs
@@ -4,11 +4,33 @@ public class Day3 : Day
 {
   public override void Part1(string input)
   {
-    throw new NotImplementedException();
+    var result = 0;
+    foreach (var line in _parseInput(input))
+    {
+      var bestPossibleCombination = int.MinValue;
+      foreach (var (firstDigit, indexOfFirstDigit) in line.Select((c, i) => (c, i)))
+      {
+        var maxPossibleAfter = int.MinValue;
+        for (var indexPast = indexOfFirstDigit + 1; indexPast < line.Length; indexPast++)
+          if (maxPossibleAfter < line[indexPast]) maxPossibleAfter = line[indexPast];
+        
+        bestPossibleCombination = Math.Max(bestPossibleCombination, (firstDigit * 10) + maxPossibleAfter);
+      }
+      result += bestPossibleCombination;
+    }
+    
+    Console.WriteLine($"Total output joltage: {result}");
   }
 
   public override void Part2(string input)
   {
     throw new NotImplementedException();
   }
+
+  private int[][] _parseInput(string input) =>
+    input
+      .Split('\n')
+      .Select(line => line.Trim().ToCharArray())
+      .Select(lineChar => lineChar.Select(e => e - '0').ToArray())
+      .ToArray();
 }
